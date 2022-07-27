59d00b3761f4cd2debf3c095f4277eec360afc1e
emir4ik-code <emirahmetov@gmail.com>
2022-07-27 17:57:53 -0400

    minor fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const controller = require("./src/controllers/controllers");
 const app = express();
 const PORT = 5000;
 
@@ -13,6 +12,5 @@ app.get("/", (req, res) => {
   res.json("Hello World!");
 });
 
-app.get("/users", controller.getAllUsers);
 app.use("/auth", require("./src/routes/auth"));
 app.use("/activities", require("./src/routes/activities"));
--- src/routes/activities.js
+++ src/routes/activities.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const activityController = require("../controllers/activityController");
 const authorization = require("../middleware/authorization")
 
-router.post("/", authorization, activityController.getAllActivities);
-router.post("/:id", authorization, activityController.getActivity);
+router.get("/", authorization, activityController.getAllActivities);
+router.get("/:id", authorization, activityController.getActivity);
 
 
 module.exports = router;
