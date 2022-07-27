3ad149117fad9bcc0c7c141b5779b3539c8c1258
emir4ik-code <emirahmetov@gmail.com>
2022-07-27 17:46:22 -0400

    activities model

---
+++ src/models/migrations/20220727213749_activities.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("activities", function (table) {
+        table.increments("id").primary();
+        table.integer("user_id").references("id").inTable("users");
+        table.string("content", 255).notNullable();
+        table.string("budget", 30);
+        table.string("people_amount", 30);
+        table.integer("likes");
+        table.integer("replies");
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("activities");
+};
