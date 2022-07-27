0425cec399593c3565b3cce510e50f00da09ee63
emir4ik-code <emirahmetov@gmail.com>
2022-07-27 19:58:38 -0400

    removed middleware for the time being to simplify client setup

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const activityController = require("../controllers/activityController");
 const authorization = require("../middleware/authorization");
 
-router.get("/", authorization, activityController.getAllActivities);
-router.get("/:id", authorization, activityController.getActivity);
-router.post("/", authorization, activityController.postActivity);
-router.delete("/:id", authorization, activityController.deleteActivity);
+router.get("/", activityController.getAllActivities);
+router.get("/:id", activityController.getActivity);
+router.post("/", activityController.postActivity);
+router.delete("/:id", activityController.deleteActivity);
 
 module.exports = router;
