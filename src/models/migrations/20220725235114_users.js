785e53a71aa31642b948fd317f9bcc9f0c5dc47a
emir4ik-code <emirahmetov@gmail.com>
2022-07-27 12:13:39 -0400

    user model finished

--- src/models/migrations/20220725235114_users.js
+++ src/models/migrations/20220725235114_users.js
@@ -6,6 +6,9 @@ exports.up = function (knex) {
   return knex.schema.createTable("users", function (table) {
     table.increments("id").primary();
     table.string("username", 255).notNullable();
+    table.string("password", 15).notNullable();
+    table.string("pronouns");
+    table.string("bio", 60)
   });
 };
 
