c39c1f302a80f2345d808d43690f7c825c1aa2c7
emir4ik-code <emirahmetov@gmail.com>
2022-07-27 20:34:45 -0400

    deployment config

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,11 +1,21 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
+const development = {
   user: "postgres",
   password: "emir",
   host: "localhost",
   port: 5432,
   database: "sample_express",
-});
+};
 
+const production = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production"
+    ? connectionProduction
+    : connectionDevelopment
+);
 module.exports = pool;
