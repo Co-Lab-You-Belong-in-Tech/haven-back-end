71008de50397a0bb9c38094a3551adb1be01bb9a
emir4ik-code <emirahmetov@gmail.com>
2022-07-27 20:57:19 -0400

    minor fix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
