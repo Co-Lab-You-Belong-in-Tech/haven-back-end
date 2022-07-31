b3d0642b35a28a7d698af692df22ed34c16e2642
emir4ik-code <emirahmetov@gmail.com>
2022-07-31 15:27:22 -0400

    formatting

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -1,13 +1,34 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('users').del()
-  await knex('users').insert([
-    {id: 10000, username: 'Jack', password: "123", email: "jack@gmail.com", pronouns:"they/them", bio:"artist"},
-    {id: 10001, username: 'John', password: "123", email: "john@gmail.com",pronouns:"he/him", bio:"musician"},
-    {id: 10002, username: 'Alice', password: "123", email: "alice@gmail.com",pronouns:"she/her",bio:"outdoor expert"}
+  await knex("users").del();
+  await knex("users").insert([
+    {
+      id: 10000,
+      username: "Jack",
+      password: "123",
+      email: "jack@gmail.com",
+      pronouns: "they/them",
+      bio: "artist",
+    },
+    {
+      id: 10001,
+      username: "John",
+      password: "123",
+      email: "john@gmail.com",
+      pronouns: "he/him",
+      bio: "musician",
+    },
+    {
+      id: 10002,
+      username: "Alice",
+      password: "123",
+      email: "alice@gmail.com",
+      pronouns: "she/her",
+      bio: "outdoor expert",
+    },
   ]);
 };
