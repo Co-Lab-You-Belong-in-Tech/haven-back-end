2dd708d4808bef0845d666624629d0ca961de3ff
emir4ik-code <emirahmetov@gmail.com>
2022-07-31 13:06:46 -0400

    add title to activity model

--- migrations/20220727213749_activities.js
+++ migrations/20220727213749_activities.js
@@ -6,6 +6,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("activities", function (table) {
     table.increments("id").primary();
     table.integer("user_id").references("id").inTable("users");
+    table.string("title", 50);
     table.string("content", 255).notNullable();
     table.string("budget", 30);
     table.string("people_amount", 30);
