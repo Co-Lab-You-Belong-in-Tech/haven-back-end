a4b1919999fac1fa3b07ba6fffb19b0956af3e26
emir4ik-code <emirahmetov@gmail.com>
2022-07-31 13:41:42 -0400

    cors enabled

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
