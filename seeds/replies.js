1b3fcc484973e9b210075ae017640c13f9370da6
emir4ik-code <emirahmetov@gmail.com>
2022-07-31 15:23:30 -0400

    replies seeds

--- seeds/replies.js
+++ seeds/replies.js
@@ -4,10 +4,10 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('replies').del()
+  await knex('replies').insert([
+    {id: 10000, user_id: 10002, activity_id: 10000, content: "Bowling sounds amazing! I'm down"},
+    {id: 10001, user_id: 10000, activity_id: 10002, content: "I can do movies tomorrow :)"},
+    {id: 10002, user_id: 10002, activity_id: 10000, content: "Bowling sounds amazing! I'm down"}
   ]);
 };
