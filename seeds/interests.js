562c01c1ae409066eac28fa87bd9472b5e17c898
emir4ik-code <emirahmetov@gmail.com>
2022-08-01 18:39:40 -0400

    interests sent along other userInfo

--- seeds/interests.js
+++ seeds/interests.js
@@ -1,13 +1,13 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('interests').del()
-  await knex('interests').insert([
-    {id: 10000, interest: 'Bowling'},
-    {id: 10001, interest: 'Music'},
-    {id: 10002, interest: 'Rock Climbing'}
+  await knex("interests").del();
+  await knex("interests").insert([
+    { id: 10000, user_id: 10000, interest: "Bowling" },
+    { id: 10001, user_id: 10001, interest: "Music" },
+    { id: 10002, user_id: 10002, interest: "Rock Climbing" },
   ]);
 };
