5724a827b4924cc9de7d88840521f3d7c13c9db4
emir4ik-code <emirahmetov@gmail.com>
2022-08-02 13:51:53 -0400

    send a friend request to a user and see incoming friend requests as a user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const friendsController = require("../controllers/friendsController");
 const authorization = require("../middleware/authorization");
 
 router.get("/", authorization, userController.getUsers);
 router.get("/profile/:id", authorization, userController.getUser);
 router.get("/me", authorization, userController.getMyUser);
+router.get(
+  "/me/friends_req/incoming",
+  authorization,
+  friendsController.getIncomingRequests
+);
+router.post("/profile/:id", authorization, friendsController.sendFriendRequest);
 
 module.exports = router;
