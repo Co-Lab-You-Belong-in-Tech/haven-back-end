63cb1caeff5e0baf5b99d6e0d1b3658c079b27ab
emir4ik-code <emirahmetov@gmail.com>
2022-08-22 20:48:53 -0400

    typo fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -66,7 +66,7 @@ class UserController {
         myUser: myUser.rows,
         myActivities: myActivities.rows,
         myInterests: myInterests.rows,
-        myMomnents: myMoments.rows
+        myMoments: myMoments.rows
       };
       res.status(200).json(userProfile);
     } catch (error) {
